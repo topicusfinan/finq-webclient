8c6dd13190d986d1e4e67b4949aedee0d1f01dc7
Christian <cmkramer@gmail.com>
2014-08-18 22:32:50 +0200

    Fix indentation and comments for the preloader controller

--- app/scripts/controllers/preloader.js
+++ app/scripts/controllers/preloader.js
@@ -2,40 +2,42 @@
 
 /**
  * @ngdoc overview
- * @name finqApp.runner:AvailableCtrl
+ * @name finqApp.runner:PreloaderCtrl
  * @description
- * # Available scenarios Controller
+ * # Application preloader
  *
- * The available controller allows a user to execute tests. It provides lists of available tests that can
- * be run, and provides the user with the ability to execute a particular test. Such a test can
- * either be run in the background or in debug mode.
+ * Explicitly preload certain content before the actual application is initialized. This ensures
+ * that required files and data has been initialized properly if that data had to be retrieved
+ * asynchronously.
+ *
+ * Application state will be changed to 'loaded' after all content has been successfully preloaded.
  */
 angular.module('finqApp')
     .controller('PreloaderCtrl', ['$state','config','translate',function ($state,config,translate) {
-    	var defaultLang = 'en';
-    	var loaded = {
-    		translations: false,
-    		config: false
-    	};
-    	translate.load(defaultLang,function() {
-    		loaded.translations = true;
-    		console.log('translations loaded for language: '+defaultLang);
-    		evalLoaded();
-    	});
+        var defaultLang = 'en';
+        var loaded = {
+           translations: false,
+           config: false
+        };
+        translate.load(defaultLang,function() {
+            loaded.translations = true;
+            console.log('translations loaded for language: '+defaultLang);
+            evalLoaded();
+        });
         config.load(function(){
-        	loaded.config = true;
-        	console.log('configuration loaded');
-        	evalLoaded();
+            loaded.config = true;
+            console.log('configuration loaded');
+            evalLoaded();
         });
         var evalLoaded = function() {
-        	var allLoaded = true;
-        	angular.forEach(loaded, function(value) {
-        		if (!value) {
-        			allLoaded = false;
-        		}
-        	});
-        	if (allLoaded) {
-        		$state.go('loaded');
-        	}
+            var allLoaded = true;
+            angular.forEach(loaded, function(value) {
+                if (!value) {
+                    allLoaded = false;
+                }
+            });
+            if (allLoaded) {
+                $state.go('loaded');
+            }
         };
     }]);
