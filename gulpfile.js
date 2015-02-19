cd276c5bf30f741dd1c8d59c589930489effaa5a
Marc Fokkert <marcfokkert@xz1.nl>
2015-02-19 15:01:54 +0100

    Cleaned gulpfile.js and added watchers for the remaining files.

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,13 @@ var sass = require('gulp-sass');
 var minimist = require('minimist');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
-var autoprefixer = require('gulp-autoprefixer');
+var autoPrefixer = require('gulp-autoprefixer');
 var csso = require('gulp-csso');
 var inject = require('gulp-inject');
 var ngAnnotate = require('gulp-ng-annotate');
 var karma = require('karma').server;
 var mainBowerFiles = require('main-bower-files');
-var scssLint = require('gulp-scss-lint');
+var scsslint = require('gulp-scss-lint');
 var notify = require('gulp-notify');
 var bourbon = require('node-bourbon');
 var neat = require('node-neat');
@@ -25,7 +25,6 @@ var merge = require('merge-stream');
 var watch = require('gulp-watch');
 var runSequence = require('run-sequence');
 var filter = require('gulp-filter');
-
 //endregion
 
 // region Config
@@ -41,30 +40,43 @@ var options = minimist(process.argv.slice(2), knownOptions);
 var env = config.env[options.env];
 //endregion
 
+// region Tasks
 gulp.task('default', function (done) {
     runSequence('clean',
         ['scripts', 'sass', 'moveFiles'], 'injectDependencies', done);
 });
-gulp.task('serve', function(done){
-    runSequence('default', 'browser-sync', done)
+gulp.task('serve', function (done) {
+    runSequence('default', 'browser-sync', done);
+});
+gulp.task('test', function (done) {
+    runSequence('default', 'karma', done);
 });
 
 gulp.task('clean', Clean);
-gulp.task('moveVendors', MoveVendorFiles);
+gulp.task('moveVendors', MoveVendorFiles().stream);
 gulp.task('moveViews', MoveViews);
 gulp.task('moveStatics', MoveStatics);
 gulp.task('moveConfig', MoveConfig);
 gulp.task('moveLang', MoveLang);
 gulp.task('moveFontAwesomeFonts', MoveFontAwesomeFonts);
 gulp.task('injectDependencies', InjectDependencies);
-gulp.task('jslint', JsLint);
+gulp.task('jshint', JsHint);
 gulp.task('scsslint', ScssLint);
 gulp.task('scripts', Scripts);
 gulp.task('sass', Sass);
 gulp.task('browser-sync', BrowserSync);
 gulp.task('karma', Karma);
 gulp.task('moveFiles', MoveFiles);
+//endregion
 
+// region Helper functions
+function swallowError(error) {
+    console.log(error.toString());
+    this.emit('end');
+}
+//endregion
+
+// region Function declarations
 /**
  * Clean
  */
@@ -85,17 +97,20 @@ function MoveFiles() {
 
 function MoveVendorFiles() {
     return gulp.src(mainBowerFiles())
-        .pipe(gulp.dest(paths.dest.vendor));
+        .pipe(gulp.dest(paths.dest.vendor))
+        .pipe(reload({stream: true}));
 }
 
 function MoveFontAwesomeFonts() {
     return gulp.src(paths.src.fafonts)
-        .pipe(gulp.dest(paths.dest.fonts));
+        .pipe(gulp.dest(paths.dest.fonts))
+        .pipe(reload({stream: true}));
 }
 
 function MoveViews() {
     return gulp.src(paths.src.views + "/**/*")
-        .pipe(gulp.dest(paths.dest.views));
+        .pipe(gulp.dest(paths.dest.views))
+        .pipe(reload({stream: true}));
 }
 
 /**
@@ -106,15 +121,14 @@ function MoveStatics() {
         paths.sourcedir + "/*",
         "!" + paths.sourcedir + "/index.html"
     ], {nodir: true, dot: true})
-        .pipe(gulp.dest(paths.basedir));
+        .pipe(gulp.dest(paths.basedir))
+        .pipe(reload({stream: true}));
 }
 
-/**
- * Move config json file
- */
 function MoveConfig() {
     return gulp.src(paths.src.scripts + "/config.json")
-        .pipe(gulp.dest(paths.dest.scripts));
+        .pipe(gulp.dest(paths.dest.scripts))
+        .pipe(reload({stream: true}));
 }
 
 /**
@@ -122,7 +136,8 @@ function MoveConfig() {
  */
 function MoveLang() {
     return gulp.src(paths.src.lang + "/**/*")
-        .pipe(gulp.dest(paths.dest.lang));
+        .pipe(gulp.dest(paths.dest.lang))
+        .pipe(reload({stream: true}));
 }
 
 /**
@@ -145,25 +160,27 @@ function InjectDependencies() {
             paths.dest.css + "/**/*",
             paths.dest.vendor + "/**/*.css"
         ], {read: false}), {ignorePath: 'build'}))
-        .pipe(gulp.dest("build"));
+        .pipe(gulp.dest("build"))
+        .pipe(reload({stream: true}));
 }
 
 /**
  * Check JS with jshint
  */
-function JsLint() {
-    return gulp.src(paths.src.scripts + "/**/*")
+function JsHint() {
+    return gulp.src(paths.src.scripts + "/**/*.js")
         .pipe(jshint())
         .pipe(jshint.reporter(stylish))
         .pipe(jshint.reporter('fail'));
 }
 
 /**
- * Check SCSS with scssLint
+ * Check SCSS with scsslint
  */
 function ScssLint() {
     gulp.src(paths.src.scss)
-        .pipe(scssLint())
+        .pipe(scsslint())
+        .pipe(scsslint.failReporter());
 }
 
 /**
@@ -177,14 +194,14 @@ function Scripts() {
         (env.mockdata ? "" : "!") + paths.src.scripts + "/mockdata/**/*",
         paths.src.scripts + "/**/*.js"
     ];
-    var sourcesOptions = {base: paths.src.scripts};
 
+    var sourcesOptions = {base: paths.src.scripts};
     var concatFileName = 'all.min.js';
 
     return gulp.src(sources, sourcesOptions)
         .pipe(sourcemaps.init({gulpWarnings: false}))
         .pipe(ngAnnotate({gulpWarnings: false}))
-        .on('error',  swallowError)
+        .on('error', swallowError)
         .pipe(gulpif(env.jsminify, uglify()))
         .pipe(gulpif(env.jsconcat, concat(concatFileName)))
         .pipe(sourcemaps.write())
@@ -192,14 +209,9 @@ function Scripts() {
         .pipe(reload({stream: true}));
 }
 
-function swallowError(error){
-    console.log(error.toString());
-    this.emit('end');
-}
-
 /**
  * Generate CSS from SASS and output to build
- * Includes autoprefixer and sourcemaps
+ * Includes autoPrefixer and sourcemaps
  * Minify if specified in current environment
  */
 function Sass() {
@@ -210,16 +222,18 @@ function Sass() {
         .pipe(sass({
             includePaths: bourbon.with(neat.includePaths)
         }))
-        .on('error',  swallowError)
+        .on('error', swallowError)
         .pipe(gulpif(env.scssminify, csso()))
-        .pipe(autoprefixer('last 1 version'))
+        .pipe(autoPrefixer('last 1 version'))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(paths.dest.css))
         .pipe(filter('**/*.css'))
         .pipe(reload({stream: true}));
 }
 
-
+/**
+ * Run Mocha tests with Karma
+ */
 function Karma(done) {
     karma.start({
         configFile: __dirname + "/test/karma.conf.js",
@@ -241,17 +255,33 @@ function BrowserSync() {
         }
     });
 
-    gulp.watch(paths.src.scripts + "/**/*.js", function(){
+    // Watch scripts
+    gulp.watch(paths.src.scripts + "/**/*.js", function () {
         runSequence('scripts', 'injectDependencies');
     });
+
+    // Watch scss
     gulp.watch(paths.src.scssfolder + "/**/*.scss", ['sass']);
-    gulp.watch("app/views/**/*.html", function(){
-        reload();
-    });
 
+    // Watch views
+    gulp.watch(paths.src.views + "/**/*.html", ['moveViews']);
 
+    // Watch index.html
+    gulp.watch(paths.src.index, ['injectDependencies']);
+
+    // Watch statics
+    gulp.watch([
+        paths.sourcedir + "/*",
+        "!" + paths.sourcedir + "/index.html"
+    ], ['moveStatics']);
+
+    // Watch config
+    gulp.watch(paths.src.scripts + "/config.json", ['moveConfig']);
+
+    // Watch lang
+    gulp.watch(paths.src.lang + "/**/*.json", ['moveLang']);
 }
-
+//endregion
 
 
 
