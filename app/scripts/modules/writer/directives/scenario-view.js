08dc3ed4ff6da048e3638094970ee9bd8e817b17
C. Kramer <c.kramer@finan.nl>
2015-03-18 11:44:28 +0100

    Add styling for the scenario title input fields and remove the need to use an "apply" button to confirm a title change

--- app/scripts/modules/writer/directives/scenario-view.js
+++ app/scripts/modules/writer/directives/scenario-view.js
@@ -16,9 +16,7 @@ angular.module('finqApp.writer.directive')
     })
     .controller('ScenarioViewCtrl', function ($scope, selectedItem, arrayOperations) {
         $scope.scenarioView = {
-            deleteItem: DeleteItem,
-            //isStepIncomplete: IsStepIncomplete,
-            applyScenarioTitle: ApplyScenarioTitle
+            deleteItem: DeleteItem
         };
 
         $scope.selectedItem = {
@@ -30,52 +28,8 @@ angular.module('finqApp.writer.directive')
         $scope.stepListHandler = '.scenario-handle';
         $scope.scenarioContentListClass = '.scenario-content-list';
 
-        //$scope.registerSortable = RegisterSortable;
-
-
         function DeleteItem(collection, index) {
             arrayOperations.removeItem(collection, index);
         }
 
-        function ApplyScenarioTitle(scenario) {
-            scenario.title = scenario.editorTitle;
-        }
-
-        //function IsStepIncomplete(step) {
-        //    // TODO create a service to evaluate this
-        //
-        //}
-
-
-        //var start;
-        //function RegisterSortable(){
-        //    $('.main-content-list').sortable({
-        //        handle: '.handle',
-        //        start: function(event,ui){
-        //            start = ui.item.index();
-        //        },
-        //        update: function(event, ui){
-        //            var animatedElements = $('.main-content-list').find('.list-animate');
-        //            animatedElements.removeClass('list-animate');
-        //            MoveObject($scope.scenarios, start, ui.item.index());
-        //            $scope.$digest(); // this digest is needed to make sure $index on scope aligns with the array
-        //            animatedElements.addClass('list-animate');
-        //        }
-        //    });
-        //    $('.scenario-content-list').eq(0).sortable({
-        //        handle: '.scenario-handle',
-        //        start: function(event, ui){
-        //            start = ui.item.index();
-        //        },
-        //        update: function(event,ui){
-        //            var animatedElements = $('.scenario-content-list').find('.list-animate');
-        //            animatedElements.removeClass('list-animate');
-        //            MoveObject(ui.item.$scope.$parent.steps, start, ui.item.index());
-        //        }
-        //    });
-        //}
-        //
-        //function MoveObject(array, originalPosition, insertPosition){
-        //    array.splice(insertPosition, 0, array.splice(originalPosition, 1)[0]);
-        //}
     });
