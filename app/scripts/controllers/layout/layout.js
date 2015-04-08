aaa315dfb04e311296be3b96b9d34f5a8f14ddab
Marc Fokkert <marcfokkert@xz1.nl>
2015-04-08 16:25:37 +0200

    Remove unnecessary functions from layout controller.

--- app/scripts/controllers/layout/layout.js
+++ app/scripts/controllers/layout/layout.js
@@ -12,17 +12,8 @@ angular.module('finqApp.controller')
     .controller('LayoutCtrl', function ($timeout, sidebar, STATE) {
         var that = this;
 
-        this.hasSidebar = function(){
-            return sidebar.getStatus() !== STATE.SIDEBAR.HIDDEN;
-        };
-
-        this.sidebarIsExpanded = function() {
-            return sidebar.getStatus() === STATE.SIDEBAR.EXPANDED;
-        };
-
         // delay the loaded indication to allow for appear effects
         $timeout(function() {
             that.loaded = true;
         },10);
-
     });
